b7ffdbab79bd9d103f83443f1305ea9a5e304862
robert-1216 <70184171+robert-1216@users.noreply.github.com>
2021-10-28 22:21:37 +0800

    test change

--- compiler.cpp
+++ compiler.cpp
@@ -49,7 +49,7 @@ void process_number(string word){
         FuncDef.push_back("Number");
     }
     else{
-        exit(1);
+        //exit(1);
     }
 }
 
@@ -97,7 +97,7 @@ void process(string word){
         FuncDef.push_back(word);
     }
     else{
-        exit(1);
+        //exit(1);
     }
 }
 
@@ -169,7 +169,7 @@ int main(int argc, char *argv[]){
         }
     }
     else{
-        exit(1);
+        //exit(1);
     }
     input.close();
     ir.close();
