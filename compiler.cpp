6ef80d5cab194c82311431371e75359cc42b479f
robert-1216 <70184171+robert-1216@users.noreply.github.com>
2021-11-02 11:41:22 +0800

    Update compiler.cpp

--- compiler.cpp
+++ compiler.cpp
@@ -53,51 +53,117 @@ void process_number(string word){
     }
 }
 
-void process(string word){
-    if (word == "int"){
-        items.push_back("define dso_local i32 ");
-        FuncDef.push_back("FuncType");
+int letter(string word, int pos){
+    string letters;
+    while (pos < word.length() && (isalnum(word[pos]) || word[pos] == '_')){
+        letters.push_back(word[pos]);
+        pos++;
     }
-    else if (word.length() >= 4 && word.substr(0, 4) == "main"){
-        items.push_back("@main");
-        FuncDef.push_back("Ident");
-        if (word.length() > 4){
-            size_t rtn_location = word.find("return", 4);
-            for (int i = 4; i < min(rtn_location, word.length()); i++){
-                process(word.substr(i, 1));
-            }
-            if (rtn_location != string::npos){
-                process(word.substr(rtn_location));
-            }
-        }
+    /*if (letters == "if"){
+        printf("If\n");
     }
-    else if (word == "(" || word == ")" || word == "{" || word == "}"){
-        items.push_back(word);
-        FuncDef.push_back(word);
+    else if (letters == "else"){
+        printf("Else\n");
     }
-    else if (word == "return"){
+    else if (letters == "while"){
+        printf("While\n");
+    }
+    else if (letters == "break"){
+        printf("Break\n");
+    }
+    else if (letters == "continue"){
+        printf("Continue\n");
+    }
+    /*else */if (letters == "return"){
+        //printf("Return\n");
         items.push_back("ret ");
-        FuncDef.push_back(word);
-    }
-    else if (isdigit(word.at(0))){
-        if (word.length() > 1 && word.at(word.length() - 1) == ';'){
-            process_number(word.substr(0, word.length() - 1));
-            process(";");
-        }
-        else if (word.length() > 2 && word.at(word.length() - 2) == ';' && word.at(word.length() - 1) == '}'){
-            process_number(word.substr(0, word.length() - 2));
-            process(";");
-            process("}");
-        }
-        else{
-            process_number(word);
-        }
-    }
-    else if (word == ";"){
-        FuncDef.push_back(word);
+        FuncDef.push_back(letters);
     }
     else{
-        exit(1);
+        //printf("Ident(%s)\n", letters.c_str());
+        if (letters == "int"){
+            items.push_back("define dso_local i32 ");
+            FuncDef.push_back("FuncType");
+        }
+        else if (letters == "main"){
+            items.push_back("@main");
+            FuncDef.push_back("Ident");
+        }
+        else{
+            exit(1);
+        }
+    }
+    return letters.length();
+}
+
+int digit(string word, int pos){
+    string number;
+    while (pos < word.length() && isalnum(word[pos])){
+        number.push_back(word[pos]);
+        pos++;
+    }
+    //printf("Number(%s)\n", number.c_str());
+    process_number(number);
+    return number.length();
+}
+
+void process(string word){
+    int pos = 0;
+    while (pos < word.length()){
+        if (isalpha(word[pos]) || word[pos] == '_'){
+            pos += letter(word, pos);
+        }
+        else if (isdigit(word[pos])){
+            pos += digit(word, pos);
+        }
+        else if (word[pos] == '(' || word[pos] == ')' || word[pos] == '{' || word[pos] == '}'){
+            /*printf("LPar\n");
+            printf("RPar\n");
+            printf("LBrace\n");
+            printf("RBrace\n");*/
+            items.push_back(string(1, word[pos]));
+            FuncDef.push_back(string(1, word[pos]));
+            pos++;
+        }
+        /*else if (word[pos] == '+'){
+            printf("Plus\n");
+            pos++;
+        }
+        else if (word[pos] == '*'){
+            printf("Mult\n");
+            pos++;
+        }
+        else if (word[pos] == '/'){
+            printf("Div\n");
+            pos++;
+        }
+        else if (word[pos] == '<'){
+            printf("Lt\n");
+            pos++;
+        }
+        else if (word[pos] == '>'){
+            printf("Gt\n");
+            pos++;
+        }
+        else if (word[pos] == '='){
+            if (pos < word.length() - 1 && word[pos + 1] == '='){
+                printf("Eq\n");
+                pos += 2;
+            }
+            else{
+                printf("Assign\n");
+                pos++;
+            }
+        }*/
+        else if (word[pos] == ';'){
+            //printf("Semicolon\n");
+            FuncDef.push_back(";");
+            pos++;
+        }
+        else{
+            //printf("Err\n");
+            exit(1);
+        }
     }
 }
 
